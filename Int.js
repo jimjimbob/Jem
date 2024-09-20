d2e5fb8694f760b862849eb816aed3a8cdeba54b
Jem <andersonkids.ka@gmail.com>
2024-09-19 22:12:23 -0500

    updated index.html
    added a loading screen in the style of a booting linux computer (added a funky funky in there)
    
    Join DedSec

---
+++ Int.js
@@ -0,0 +1,32 @@
+const loadingMessages = [
+    "Booting up Err's System...",
+    "Checking system integrity...",
+    "Loading required modules...",
+    "Establishing secure connections...",
+    "Initializing graphical interface...",
+    "Loading user preferences...",
+    "Engaging security protocols...",
+    "DedSec system active...",
+    "Loading complete. Welcome to Err's World..."
+];
+
+let currentMessageIndex = 0;
+const loadingTextElement = document.getElementById('loading-text');
+
+function displayNextMessage() {
+    if (currentMessageIndex < loadingMessages.length) {
+        const newMessage = loadingMessages[currentMessageIndex] + "\n";
+        loadingTextElement.textContent += newMessage;
+        currentMessageIndex++;
+
+        setTimeout(displayNextMessage, 1000);
+    } else {
+        setTimeout(() => {
+            document.getElementById('loading-screen').style.display = 'none';
+            document.getElementById('main-content').style.display = 'block';
+        }, 1000);
+    }
+}
+
+
+window.onload = displayNextMessage;
